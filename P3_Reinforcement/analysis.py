24c85dd02a3e57831a1538f7d8d4106f66fff0d3
Maithreyi <maithreyi220288@gmail.com>
2018-03-22 23:22:43 -0700

    Analysis for qn 3 done

--- P3_Reinforcement/analysis.py
+++ P3_Reinforcement/analysis.py
@@ -21,41 +21,42 @@
 
 def question2():
     answerDiscount = 0.9
-    answerNoise = 0.0
+    answerNoise = 0.01
     return answerDiscount, answerNoise
 
 def question3a():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerDiscount = 0.2
+    answerNoise = -0.1
+    answerLivingReward = -0.8
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3b():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+
+    answerDiscount = 0.4
+    answerNoise = 0.2
+    answerLivingReward = -0.3
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3c():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerDiscount = 0.8
+    answerNoise = 0.01
+    answerLivingReward = 0.1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3d():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerDiscount = 0.7
+    answerNoise = 0.4
+    answerLivingReward = -0.01
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3e():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerDiscount = 0.01
+    answerNoise = 0.7
+    answerLivingReward = 0.9
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
